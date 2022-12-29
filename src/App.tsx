aaa529df5ee211fcb6c34794db0ff191abce36a0
yagoh2303 <yago.henriquess@hotmail.com>
2022-12-28 22:56:16 -0300

    active a tmdb

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import CONST from './data/constants';
 
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
@@ -7,16 +8,41 @@ import Carousel from './components/Carousel';
 import 'slick-carousel/slick/slick.css'; 
 import 'slick-carousel/slick/slick-theme.css';
 
-const App = () =>  
- (
+const App = () =>  {
+
+  const { URL, APIKEY } = CONST;
+  
+
+  const [movies, setMovies] = useState();
+
+    useEffect (() => {
+      const fetchData = async () => {
+
+        const response = await fetch (`${URL}/discover/movie?api_key=${APIKEY}&language=pt-BR&sort_by=popularity.desc`);
+
+        const data = await response.json();
+
+        setMovies(data);
+      }
+        fetchData(); 
+      
+    }, []);
+
+    useEffect(() => movies && console.log(movies), [ movies ]);
+  
+  
+  return(
     <div className="m-auto antialised font-sans bg-gray-900 text-white">
-      <Hero />
+      <Hero {...movies?.results[0]} />
       <Navbar /> 
       <Carousel />
       <Carousel />
       <Carousel />
     </div>
   );
+
+}
+
 
 
 export default App;
---
+++ src/data/constants.tsx
@@ -0,0 +1,9 @@
+const URL ='https://api.themoviedb.org/3'
+const IMAGEURL ='https://image.tmdb.org/t/p'
+const APIKEY ='36882cd3ae6725b9faf3f09bcc03981a'
+
+export default {
+    URL, 
+    IMAGEURL,
+    APIKEY
+}
