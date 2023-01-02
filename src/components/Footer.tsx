95c6511848f12213f54628c597b52119e85d0621
yagoh2303 <yago.henriquess@hotmail.com>
2023-01-02 14:46:45 -0300

    added css footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookSquare, faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
+import './Footer.css'
 
 const Footer = () =>  {
     const today = new Date();
 return(
-    <footer>
+    <footer className='px-12 mt-36 pb-4 text-gray-500'>
  
-            <FontAwesomeIcon icon={faFacebook} />
-            <FontAwesomeIcon icon={faInstagram} />
-            <FontAwesomeIcon icon={faTwitter} />
-            <FontAwesomeIcon icon={faYoutube} />
+            <FontAwesomeIcon className="link mr-8" icon={faFacebookSquare} size='2x' />
+            <FontAwesomeIcon className="link mr-8" icon={faInstagram}  size='2x'/>
+            <FontAwesomeIcon className="link mr-8" icon={faTwitter} size='2x'/>
+            <FontAwesomeIcon className="link" icon={faYoutube} size='2x'/>
 
-        <div>
+        <div className='links grid grid-cols-4 gap-2 pt-4'>
+            
             <a href="#">Idiomas e legendas</a>
             <a href="#">Audiodescrição</a>
             <a href="#">Centro de ajuda</a>
@@ -28,7 +31,7 @@ return(
             <a href="#">Entre em contato</a>
         </div>
 
-        <p>© 1990 - {today.getFullYear()} Imagiflix, Inc.</p>
+        <p className="my-4">© 1990 - {today.getFullYear()} Imagiflix, Inc.</p>
     </footer>
 );
 };
